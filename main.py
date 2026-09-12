65494effa21ffa5da63de54b0c67f950f785eb18
amine69704-ctrl <amine69704@gmail.com>
2026-09-12 12:10:16 +0600

    Initialize Discord bot with ping command

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot Ready! Logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+# Token loads from environment variable or .env
+TOKEN = os.getenv('DISCORD_TOKEN')
+if TOKEN:
+    bot.run(TOKEN)
+else:
+    print("Error: DISCORD_TOKEN not found!")
